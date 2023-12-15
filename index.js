712110c89265faad0d7f3e1cf72e211d16bd3e87
mohamedbashir <mohaedbashir45@gmail.com>
2023-12-15 20:27:28 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Mohamed bashir';
+const welcomeMessage = 'Welcome to Flatbook, Kifah Mohamed bashir!';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, MOHAMED BASHIR!";
+const shortGreeting = "Welcome, M!";
